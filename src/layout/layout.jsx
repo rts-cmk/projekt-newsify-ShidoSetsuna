0d58473df3fd633da2089869cc9ac78d26fdc448
Valdemar Larsen <shido813@gmail.com>
2025-10-21 12:41:42 +0200

    Added navbar

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
+import Nav from "../components/nav/nav.jsx";
 
 export default function Layout({ onLogout }) {
   return (
     <>
       <Outlet />
+      <Nav />
     </>
   );
 }
